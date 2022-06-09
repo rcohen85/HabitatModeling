aced9497faeab0efc57208d5fddc3a8cbbf610ae
Rebecca Cohen <30301737+rcohen85@users.noreply.github.com>
2022-06-08 17:12:20 -0700

    Fixed bug which was replotting the same timeseries over and over

--- calculate_wklymean_covars.R
+++ calculate_wklymean_covars.R
@@ -3,25 +3,17 @@ library(zoo)
 library(gridExtra)
 library(stringr)
 
-data = read.csv("E:/ModelingCovarData/Master_DFs/Cuvier_masterDF.csv")
-outDir = "E:/ModelingCovarData/CovarTS_Plots"
+data = read.csv("J:/Chpt_3/ModelData/Cuvier_masterDF.csv")
+outDir = "J:/Chpt_3/CovarPlots"
 
 covarList = c("Chl0",
               "SSH0",
               "FSLE0",
               "EKE0",
-              "AEddyDist",
-              "CEddyDist",
+              "AEddyDist0",
+              "CEddyDist0",
               "Sal0",
-              "Sal700",
-              "Temp0",
-              "Temp700",
-              "VelMag0",
-              "VelMag700",
-              "VelAsp0",
-              "VelAsp700",
-              "GSLat",
-              "GSDist")
+              "Temp0")
 
 sites = c("HZ","OC","NC","BC","WC","NFC","HAT","GS","BP","BS")
 k=7
@@ -31,33 +23,23 @@ dateMarks = c(seq.Date(startDate,endDate,by="year"),endDate)
 
 for (j in 1:length(sites)){
   siteInd = which(!is.na(str_match(data$Site,sites[j])))
-  # if (sum(data$Pres[siteInd]>0)>25){
-    plotDF = data.frame(Pres = rollmean(data$Pres[siteInd],k=k,fill=NA),
-                        Date = as.Date(rollmean(data$Date[siteInd],k=k,fill=NA),origin="1970-01-01"))
+    plotDF = data.frame(Date = as.Date(rollmean(data$Date[siteInd],k=k,fill=NA),origin="1970-01-01"))
     for (m in 1:(length(dateMarks)-1)){ # for each year
       plotList = list()
       st = dateMarks[m]
       ed = dateMarks[m+1]
-      # plotList[['Pres']] = ggplot(plotDF,aes(x=Date) # plot presence in this year
-      # )+geom_line(aes(y=Pres)
-      # )+scale_x_continuous(breaks=c(seq.Date(from=st,to=ed,by="quarter"))
-      # )+coord_cartesian(xlim=c(st,ed)
-      # )+ggtitle('Presence'
-      # )+labs(x=NULL,y=NULL
-      # )+theme(plot.margin=margin(c(0,43,0,0),unit="pt"))
       for (i in 1:length(covarList)){ # plot single layer covars in this year
         plotDF$Covar = rollmean(data[[covarList[i]]][siteInd],k=k,fill=NA)
         plotList[[covarList[i]]] = ggplot(plotDF,aes(x=Date)
-        )+geom_line(aes(y=plotDF$Covar)
+        )+geom_line(aes(y=Covar)
         )+scale_x_continuous(breaks=c(seq.Date(from=st,to=ed,by="quarter"))
         )+coord_cartesian(xlim=c(st,ed)
         )+ggtitle(covarList[i]
         )+labs(x=NULL,y=NULL
         )+theme(plot.margin=margin(c(0,43,0,0),unit="pt"))
       }
-      png(filename = paste(outDir,"/",sites[j],"_",substr(st,1,4),"-",substr(ed,1,4),"_rm.png",sep=""),width=3000,height=2000)
-      grid.arrange(grobs=plotList,ncol=1,nrow=length(covarList)+1,top=paste('Covars at',sites[j]))
+      png(file=paste(outDir,"/",sites[j],"_",substr(st,1,4),"-",substr(ed,1,4),"_rm.png",sep=""),width=1500,height=1000)
+      grid.arrange(grobs=plotList,ncol=1,nrow=length(covarList),top=sites[j])
       while (dev.cur()>1) {dev.off()}
     }
-  # }
 } 
