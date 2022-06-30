f532e8acbcbeac6e54e41cefd187a19b349833c3
Rebecca Cohen <30301737+rcohen85@users.noreply.github.com>
2022-06-30 09:43:10 -0700

    Added PCA analysis

--- clusterCovars.R
+++ clusterCovars.R
@@ -11,6 +11,7 @@ masterDF = masterDF[-badRows,]
 # create training data with no site labels
 trainDat = masterDF[,3:14]
 
+# K-means clustering -----------------------
 # determine optimal # clusters
 HartsRule = FitKMeans(trainDat, max.clusters=20, nstart=25)
 PlotHartigan(HartsRule)
@@ -36,3 +37,27 @@ plot(K, data=masterDF[,2:14], class="Site")
 # just cluster median covar values
 medMat = matrix(nrow=length(unique(masterDF$Site)),ncol=)
 
+
+# PCA -------------------------------------
+sites = unique(masterDF$Site)
+masterDF$SiteFac = factor(masterDF$Site,levels=sites)
+pc = prcomp(trainDat,center=TRUE,scale=TRUE)
+
+g <- ggbiplot(pc,
+              obs.scale = 1,
+              var.scale = 1,
+              groups = masterDF$SiteFac,
+              ellipse = TRUE,
+              circle = FALSE,
+              ellipse.prob = 0.66,
+              varname.size=4)
+g = g + scale_color_manual(values=c("#f05a43","#db51a4","#de76f5","#9b85f2",
+                                    "#56b4fc","#2fc7cc","#3dd17d","#5aba30",
+                                    "#bbc22f","#d6a10f"))
+g <- g + theme(legend.direction = 'vertical',
+               legend.position = 'right')
+
+png(filename=paste(getwd(),'/EcologicalNichePlots/OceanographyPCA.png',sep=""),
+    height=600, width=600,units="px",res=100)
+print(g)
+while (dev.cur()>1) {dev.off()}
